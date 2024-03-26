37011bc7db89590a2c9cf63ce7de49463106c05f
Dennis Benz <dennis.benz@uos.de>
2024-03-26 16:06:23 +0100

    Increase max token limit to 500

--- controllers/api.php
+++ controllers/api.php
@@ -218,7 +218,7 @@ class ApiController extends PluginController
                     'content' => $prompt,
                 ],
             ],
-            'max_tokens' => 200,
+            'max_tokens' => 500,
             'temperature' => 0.9,
             'top_p' => 1,
             'frequency_penalty' => 0,
